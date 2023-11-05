ad95b441e06aaf398873aa554330ed1dcd89cd3e
Vinit <vinitsk99@gmail.com>
2023-11-05 22:45:41 +0530

    add js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const mobileMenuButton = document.getElementById("mobile-menu");
-const nav = document.querySelector(".nav");
-
-mobileMenuButton.addEventListener("click", () => {
-    nav.classList.toggle("show");
-});
+//Future update
